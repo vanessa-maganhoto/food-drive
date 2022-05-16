92f243781d1b9820340eab793d75bc69b730b057
vanessa-maganhoto <vanessammaganhoto@gmail.com>
2022-05-16 15:08:08 -0300

    Criação do projeto

---
+++ backend/src/main/java/com/vanessamatos/fooddrive/FoodDriveApplication.java
@@ -0,0 +1,14 @@
+package com.vanessamatos.fooddrive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class
+FoodDriveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodDriveApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/vanessamatos/fooddrive/FoodDriveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vanessamatos.fooddrive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodDriveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
